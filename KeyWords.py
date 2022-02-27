8e1ef18157d855f0b48195ec6a05a2df226ff2e1
Jimin Kim <0305jimin@gmail.com>
2022-02-27 23:25:35 +0900

    recap

---
+++ KeyWords.py
@@ -0,0 +1,20 @@
+"""
+✔️ decorator(@) ;
+    looks for a function right below and decorate it.
+✔️ route
+✔️ placeholder (<>)
+✔️ render_template
+    {{ variable names }}
+    {% for item in items %} {% endfor %}
+✔️ redirect
+✔️ request :
+    everytime I go to the website, there's a request. 
+✔️ arguments
+    query arguments
+    request.args
+    /report?args=1&args=2
+✔️ flask :
+    pertinent for small projects like this.
+✔️ django :
+    pertinent for using db, admin, ...
+"""
